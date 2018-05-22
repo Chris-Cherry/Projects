54567b0a3974bcf03d252e25cf75f07852dad588
Chris-Cherry <ccherry.6574@gmail.com>
2018-05-22 11:47:39 -0400

    Added prediction

--- ClassifyTitanic/decisionTree/decisionTree.py
+++ ClassifyTitanic/decisionTree/decisionTree.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 import math
 
@@ -70,25 +69,38 @@ class decisionTree:
     def fit(self, X, y):
         self.tree = self.buildTree(X, y)
 
+    def predict(self, X):
+        assert self.tree is not None, "Run fit before predict!"
+        predictions = []
+        # Step through the samples
+        for sample_index in range(X.shape[0]):
+            predictions.append(
+                self.navigateBranch(X.iloc[sample_index, :], self.tree)
+            )
+        return (predictions)
+
     def calcInformationGain(self, X, y, iFeat, val):
-        baseP = float(sum(y)) / len(y)
-        baseQ = 1 - baseP
-        baseEntropy = -baseP * math.log(baseP+.001, 2) - baseQ * math.log(baseQ+.001, 2)
+        base_p = float(sum(y)) / len(y)
+        base_q = 1 - base_p
+        base_entropy = -base_p * math.log(base_p + .001, 2) \
+                       - base_q * math.log(base_q + .001, 2)
 
-        leftIndex = X.iloc[:, iFeat] >= val
-        leftP = float(sum(y.loc[leftIndex])) / len(y.loc[leftIndex])
-        leftQ = 1 - leftP
-        leftEntropy = -leftP * math.log(leftP+.001, 2) - leftQ * math.log(leftQ+.001, 2)
+        left_index = X.iloc[:, iFeat] >= val
+        left_p = float(sum(y.loc[left_index])) / len(y.loc[left_index])
+        left_q = 1 - left_p
+        left_entropy = -left_p * math.log(left_p + .001, 2) \
+                       - left_q * math.log(left_q + .001, 2)
 
-        rightIndex = X.iloc[:, iFeat] < val
-        rightP = float(sum(y.loc[rightIndex])) / len(y.loc[rightIndex])
-        rightQ = 1 - rightP
-        rightEntropy = -rightP * math.log(rightP+.001, 2) - rightQ * math.log(rightQ+.001, 2)
+        right_index = X.iloc[:, iFeat] < val
+        right_p = float(sum(y.loc[right_index])) / len(y.loc[right_index])
+        right_q = 1 - right_p
+        right_entropy = -right_p * math.log(right_p + .001, 2) \
+                        - right_q * math.log(right_q + .001, 2)
 
-        postEntropy = leftEntropy * float(sum(leftIndex)) / len(y) \
-                      + rightEntropy * float(sum(rightIndex)) / len(y)
+        post_entropy = left_entropy * float(sum(left_index)) / len(y) \
+                       + right_entropy * float(sum(right_index)) / len(y)
 
-        return (baseEntropy - postEntropy)
+        return base_entropy - post_entropy
 
     def buildTree(self, X, y, level=0):
         best_split = {}
@@ -100,12 +112,11 @@ class decisionTree:
             for iFeat in range(X.shape[1]):
                 featValues = X.iloc[:, iFeat]
                 uniqueVals = featValues.unique()
-                print(np.nditer(uniqueVals))
                 for val in uniqueVals:
                     # If there is no split made then skip
                     leftIndex = X.iloc[:, iFeat] >= val
                     rightIndex = X.iloc[:, iFeat] < val
-                    if sum(leftIndex)==0 or sum(rightIndex)==0:
+                    if sum(leftIndex) == 0 or sum(rightIndex) == 0:
                         continue
 
                     # If there's actually a split made, get information gain
@@ -114,7 +125,6 @@ class decisionTree:
                         best_info_gain = info_gain
                         best_split['Feature Index'] = iFeat
                         best_split['Value'] = val
-
 
                         best_split['leftY'] = y[leftIndex]
                         best_split['leftX'] = X[leftIndex]
@@ -135,36 +145,62 @@ class decisionTree:
             return (node(Ltree=Ltree, Rtree=Rtree, thresh=best_split['Value'],
                          featIndex=best_split['Feature Index']))
 
+    def navigateBranch(self, sample_vec, node):
+        if node.classification is not None:
+            return node.classification
+        elif node.thresh is not None:
+            sample_value = sample_vec[node.featIndex]
+            if sample_value >= node.thresh:
+                prediction = self.navigateBranch(sample_vec, node.Ltree)
+                return prediction
+            elif sample_value < node.thresh:
+                prediction = self.navigateBranch(sample_vec, node.Rtree)
+                return prediction
+            else:
+                print("Sample value didn't meet threshold expectation")
+        else:
+            print("Node classification and threshold both not defined")
+
 
 def main():
-    trainingSet = pd.read_csv(
+    training_set = pd.read_csv(
         '~/Projects/ClassifyTitanic/data/train.csv',
         quotechar='"'
     )
-    testSet = pd.read_csv(
+    test_set = pd.read_csv(
         '~/Projects/ClassifyTitanic/data/test.csv',
         quotechar='"'
     )
 
     # Convert Sex to binary
-    trainingSet['Sex'] = trainingSet['Sex'].replace('male', 1)
-    trainingSet['Sex'] = trainingSet['Sex'].replace('female', -1)
-    testSet['Sex'] = testSet['Sex'].replace('male', 1)
-    testSet['Sex'] = testSet['Sex'].replace('female', -1)
+    training_set['Sex'] = training_set['Sex'].replace('male', 1)
+    training_set['Sex'] = training_set['Sex'].replace('female', -1)
+    test_set['Sex'] = test_set['Sex'].replace('male', 1)
+    test_set['Sex'] = test_set['Sex'].replace('female', -1)
 
     # Get survival data
-    Train_Survival = trainingSet['Survived']
+    train__survival = training_set['Survived']
+    test_ids = test_set['PassengerId']
 
     # Get rid of non-informative columns
-    trainingSet = trainingSet.drop(['PassengerId', 'Survived', 'Name', 'Ticket',
-                                    'Cabin', 'Embarked'], axis=1)
-    testSet = testSet.drop(['PassengerId', 'Name', 'Ticket', 'Cabin',
-                            'Embarked'], axis=1)
+    training_set = training_set.drop(['PassengerId', 'Survived', 'Name', 'Ticket',
+                                      'Cabin', 'Embarked'], axis=1)
+    test_set = test_set.drop(['PassengerId', 'Name', 'Ticket', 'Cabin',
+                              'Embarked'], axis=1)
+
+    # The data must be cleaned of Na values before running. We will
+    # simply replace the missing values with the mean of the column
+    # for this simple example.
+
+    training_set = training_set.apply(lambda x: x.fillna(x.mean()))
+    test_set = test_set.apply(lambda x: x.fillna(x.mean()))
 
     tree = decisionTree(depth=10)
-    tree.fit(X=trainingSet, y=Train_Survival)
-
-    print('hi')
+    tree.fit(X=training_set, y=train__survival)
+    predictions = tree.predict(test_set)
+    predictions = map(int, predictions)
+    predictions = pd.DataFrame({'Survived': predictions, 'PassengerId': test_ids})
+    predictions.to_csv('~/Projects/ClassifyTitanic/decisionTree/predictions.csv', index=False)
 
 
 if __name__ == "__main__":
